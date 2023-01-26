57d32c586ec2930e30fa56196145b80d19bb1f22
Tatiana <tania1-4@mail.ru>
2023-01-26 19:32:53 +0300

    add Example006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 6;
+int b = 4;
+int c = 8;
+int d = 3;
+int e = 1;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
