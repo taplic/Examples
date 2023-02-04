644f8d346cd9396e156d5f310448484c3a4e89ae
Tatiana <tania1-4@mail.ru>
2023-02-04 22:55:13 +0300

    добавила новые файлы

---
+++ Example011/Program.cs
@@ -0,0 +1,27 @@
+﻿/* void FillAraay(int[] collection);
+{
+  int length = Collection.length;
+  int index = 0;
+}
+while (index < length)
+{
+collection[index] = new Random().Next(1, 10);
+index++;
+}
+void PrintArray(int[] col);
+{
+int count = Col.length;
+int position = 0;
+
+while (position < count)
+{
+Console.WriteLine(col[position]);
+position++;
+}
+
+}
+int[] array = new int {10};
+FillAraay(array);
+PrintArray(array);
+*/ 
+
---
+++ Examplle012/Program.cs
@@ -0,0 +1,158 @@
+﻿void Method1()
+{
+  Console.WriteLine("Автор...");
+}
+//Method1();
+
+void Method2(string msg)
+{
+  Console.WriteLine(msg);
+}
+//Method2("Текст сообщения");
+
+void Method21(string msg, int count)
+{
+  int i = 0;
+  while (i < count)
+  {
+    Console.WriteLine(msg);
+    i++;
+  }
+}
+//Method21(msg:"Текст сообщения", count: 4);
+
+int Method3()
+{
+  return DateTime.Now.Year;
+}
+int year = Method3();
+//Console.WriteLine(year);
+
+/* string Method4(int count, string text)
+{
+  int i = 0;
+  string result = String.Empty;
+  while (i < count)
+  {
+    result = result + text;
+    i++;
+  }
+  return result;
+}
+string res = Method4(10, "abcd");
+Console.WriteLine(res);
+*/
+
+string Method4(int count, string text)
+{
+  string result = String.Empty;
+  for (int i = 0; i < count; i++)
+  {
+    result = result + text;
+  }
+  return result;
+}
+string res = Method4(10, "z");
+//Console.WriteLine(res);
+
+//for (int i = 2; i <= 10; i++)
+//{
+// for (int j = 2; j <= 10; j++)
+// {
+//   Console.WriteLine($"{i} * {j} = {i * j}");
+// }
+// Console.WriteLine();
+//}
+
+//=====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёрточками,
+// маленькие буквы “к” заменить большими “К”,
+// а большие “С” маленькими “с”.
+// Ясна ли задача?
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+ + " ежели бы вас послали вместо нашего милого Винценгероде,"
+ + "вы бы взяли приступом согласие прусского короля."
+ + " Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace(string text, char oldValue, char newValue)
+{
+  string result = String.Empty;
+  int length = text.Length;
+  for (int i = 0; i < length; i++)
+  {
+    if (text[i] == oldValue) result = result + $"{newValue}";
+    else result = result + $"{text[i]}";
+  }
+
+  return result;
+}
+/* string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+string newText1 = Replace(text, 'к', 'К');
+Console.WriteLine(newText1);
+Console.WriteLine();
+string newText2 = Replace(text, 'с', 'С');
+Console.WriteLine(newText2);
+*/
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+  int count = array.Length;
+  for (int i = 0; i < count; i++)
+  {
+    Console.Write($"{array[i]} ");
+  }
+  Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+  for (int i = 0; i < array.Length - 1; i++)
+  {
+    int minPosition = i;
+    for (int j = i + 1; j < array.Length; j++)
+    {
+      if (array[j] < array[minPosition]) minPosition = j;
+    }
+    int temporary = array[i];
+    array[i] = array[minPosition];
+    array[minPosition] = temporary;
+  }
+}
+//PrintArray(arr);
+//SelectionSort(arr);
+
+//PrintArray(arr);
+
+int[] arr1 = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray1(int[] array)
+{
+  int count = array.Length;
+  for (int i = 0; i < count; i++)
+  {
+    Console.Write($"{array[i]} ");
+  }
+  Console.WriteLine();
+}
+void SelectionSort1(int[] array)
+{
+  for (int i = 0; i < array.Length - 1; i++)
+  {
+    int maxPosition = i;
+    for (int j = i + 1; j < array.Length; j++)
+    {
+      if (array[j] > array[maxPosition]) maxPosition = j;
+    }
+    int temporary = array[i];
+    array[i] = array[maxPosition];
+    array[maxPosition] = temporary;
+  }
+}
+PrintArray1(arr1);
+SelectionSort1(arr1);
+
+PrintArray1(arr1);
